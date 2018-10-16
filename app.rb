d7da7af82846283508fbef3f70c8768c4562f6e1
valeria campilongo <cvaleriac@hotmail.it>
2018-10-16 19:31:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,11 +5,11 @@ class App < Sinatra::Base
   "My name is Valeria"
 end
 
-get '/hometown' do	
-    "My hometown is NY"	
-  end	
-   get '/favorite-song' do	
-    "My favorite song is Volare"	
+get '/hometown' do
+    "My hometown is NY"
+  end
+   get '/favorite-song' do
+    "My favorite song is Volare"
   end
 
 end
